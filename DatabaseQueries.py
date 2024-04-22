d1eedf756eaf3b5aecedd66ebeb54b353f30e3eb
thomasgon06 <164603570+thomasgon06@users.noreply.github.com>
2024-04-21 19:32:55 -0500

    Added New Queries

--- DatabaseQueries.py
+++ DatabaseQueries.py
@@ -4,11 +4,14 @@ import string
 from pymongo import MongoClient
 import bson
 from bson.json_util import dumps
+import matplotlib.pyplot as plt
+
 
 connection_string = "mongodb://localhost:27017/"
 client = MongoClient(connection_string)
 current_db = client.SensorData
 collection = current_db.SensorInfo
+meta = current_db.Metadata
 
 #docs is a list of dictionaries
 
@@ -72,4 +75,68 @@ def write_to_json(docs: list, file_path: string):
     with open(file_path, 'w') as file:
         file.write(json_data)
     file.close()
+
+def plot_recent_voltage(ModuleNumber: float):
+    metacollec = meta.find()
+    for m in metacollec:
+        mostrecent = m["Most Recent"]
+        print(type(mostrecent))
+
+    startofrange = mostrecent.replace(hour=0, minute=0, second=0, microsecond=0)
+    endofrange = mostrecent.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+    docs = list(collection.find({ "Module #" : ModuleNumber, "Date Recorded" : {'$gte': startofrange,'$lte': endofrange}}, {'_id': 0}).sort('Date Recorded', -1))
+    for doc in docs:
+        if 'Date Recorded' in doc:  
+            doc['Date Recorded'] = doc['Date Recorded'].strftime('%m/%d/%Y %I:%M:%S.%f %p')
+    x = []
+    y = []
+
+    for doc in docs:
+        x.append(doc.get("Date Recorded"))
+        y.append(doc.get("Voltage"))
+
+    # Plotting the data with dots
+    plt.scatter(x, y, color='blue', marker='o')
+    plt.xticks([])
+    #plt.yticks(range(0, int(max(y)) + 2, 1))
+
+    # Adding labels and title
+    plt.ylabel('Voltage')
+    plt.title('Voltage Plot for Module ' + str(ModuleNumber))
+
+    # Display the plot
+    plt.show()
+
+    return docs
+
+def misbehaving():
+    metacollec = meta.find()
+    for m in metacollec:
+        mostrecent = m["Most Recent"]
+
+    startofrange = mostrecent.replace(hour=0, minute=0, second=0, microsecond=0)
+    endofrange = mostrecent.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+    badtemps = list(collection.find({"Temperature": {"$not": {"$gte": 18, "$lte": 28}}, "Date Recorded" : {'$gte': startofrange,'$lte': endofrange}},{'_id': 0}).sort('Date Recorded', -1))
+    print(len(badtemps))
+    badvoltages = list(collection.find({"Voltage": {"$not": {"$gte": 1000, "$lte": 1400}}, "Date Recorded" : {'$gte': startofrange,'$lte': endofrange}},{'_id': 0}).sort('Date Recorded', -1))
+    print(len(badvoltages))
+    badadc = list(collection.find({"ADC": {"$not": {"$gte": 1200, "$lte": 1250}}, "Date Recorded" : {'$gte': startofrange,'$lte': endofrange}},{'_id': 0}).sort('Date Recorded', -1))
+    print(len(badadc))
+    combined = badtemps + badvoltages + badadc
+    unique = []
+    [unique.append(x) for x in combined if x not in unique]
+    return unique
+
+    
+
+
+    
+
+
+
+
+    
+
     
--- demo.py
+++ demo.py
@@ -1,21 +1,33 @@
 import sys
 import os
-#change paths to appropriate paths for your system...
-sys.path.append('.../Database_Script')
+sys.path.append('/Users/tommy/desktop/Database_Script')
 import DatabaseQueries
 import pprint
+import matplotlib.pyplot as plt
+import random
 
 printer = pprint.PrettyPrinter()
 
 
 def main():
-    docs = DatabaseQueries.find_voltage(9.554253567558156)
-    DatabaseQueries.write_to_json(docs, ".../data_files/data.json")
-    DatabaseQueries.write_to_bson(docs, ".../data_files/output.bson")
+    docs = DatabaseQueries.all_records()
+    #print(len(docs))
+    #DatabaseQueries.plot_recent_voltage(43)
+    #for doc in docs:
+      # printer.pprint(doc)
+       #print()
 
-    for doc in docs:
-        printer.pprint(doc)
-        print()
+    
+    DatabaseQueries.write_to_json(docs, "/Users/tommy/desktop/Database_Script/data_files/data.json")
+    #DatabaseQueries.write_to_bson(docs, "/Users/tommy/desktop/Database_Script/data_files/output.bson")
+
+    #docs = DatabaseQueries.plot_voltage(339)
+   
+
+   
+
+
+   
 
 
 
